b1b022f00c678b28d26cf5b0513a25bf4434cfac
NishantBelwal-7 <63443421+NishantBelwal-7@users.noreply.github.com>
2021-03-26 23:14:13 +0530

    Update code.py
    Hi 
    You can commit this change as it can be used for multiple time an can create multiple qr at same time

--- code.py
+++ code.py
@@ -4,9 +4,17 @@ qr=qrcode.QRCode(    # define the structure and size of that Qrcode structure.
     box_size=10,
     border=6
 )
-data="www.linkedin.com/in/priya-mittal-83560619b"
-qr.add_data(data)
-qr.make(fit=True )
-img=qr.make_image(fill="black",back_color="white")
-img.save("Qrcode_linkdin.png")        
+print("Welcome to QR code generator\n\n")
+choice='y'
+while(choice.lower()=="y"):
+    data=input("Enter the website/link for which you want to make QR code : ")
+    name = input("Enter the name you want to save the QR code : ")
+    qr.add_data(data)
+    qr.make(fit=True )
+    img=qr.make_image(fill="black",back_color="white")
+    img.save(name+'.png')        
+    choice = input("Want to continue (y) or (n) : ")
+
+print("Thank you for using our service ")
+
     
